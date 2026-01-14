2bdefb968c26d1fbb11711a89f8c4e2c254d8dfd
Votre Nom <votre.email@isep.fr>
2026-01-14 16:31:13 +0100

    Ajout des classes Entity et Enemy

---
+++ entities/entity.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        #pour l'instant rectangle a mettre sprite ici après
+        self.image = pygame.Surface((width, height))
+        self.image.fill((255, 0, 255))  # rose
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 0
+
+    def update(self):
+        # Méthode pr enfants
+        pass
+
+    def draw(self, screen):
+        # draw image a pos
+        screen.blit(self.image, self.rect)
